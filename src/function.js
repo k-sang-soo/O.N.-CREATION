bc219588963aa728ddd4cf5bdd4d9bdfc2d967cf
k-sang-soo <wsckm1@gmail.com>
2020-10-06 14:02:37 +0900

    자동 슬라이드로 변경

--- src/function.js
+++ src/function.js
@@ -59,14 +59,14 @@ function slider() {
         speed: 200, // 다음 버튼 누르고 다음 화면 뜨는데까지 걸리는 시간(ms)
         arrows: false, // 옆으로 이동하는 화살표 표시 여부
         dots: false, // 스크롤바 아래 점으로 페이지네이션 여부
-        autoplay: false, // 자동 스크롤 사용 여부
+        autoplay: true, // 자동 스크롤 사용 여부
         autoplaySpeed: 2000, // 자동 스크롤 시 다음으로 넘어가는데 걸리는 시간 (ms)
         pauseOnHover: false, // 슬라이드 이동	시 마우스 호버하면 슬라이더 멈추게 설정
         vertical: false, // 세로 방향 슬라이드 옵션
         prevArrow: "<button type='button' class='slick-prev'>Previous</button>", // 이전 화살표 모양 설정
         nextArrow: "<button type='button' class='slick-next'>Next</button>", // 다음 화살표 모양 설정
         dotsClass: 'slick-dots', //아래 나오는 페이지네이션(점) css class 지정
-        draggable: true, //드래그 가능 여부
+        draggable: false, //드래그 가능 여부
 
         responsive: [
             // 반응형 웹 구현 옵션
