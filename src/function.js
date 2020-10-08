901cbf8b91e85c8ff474e4a6336cb256158aef41
k-sang-soo <wsckm1@gmail.com>
2020-10-08 13:10:11 +0900

    console 삭제 font css 수정4

--- src/function.js
+++ src/function.js
@@ -8,16 +8,13 @@ function scrollIndicator() {
     scrollIndicatorEffect.style.left = percentage + '%';
     if (scrollIndicatorEffect.style.left <= '0.5%') {
         scrollIndicatorEffect.style.left = '-15px';
-        console.log('작다');
     }
 
     if (scrollIndicatorEffect.style.left >= '99.4%') {
-        console.log('크다');
         scrollIndicatorEffect.style.display = 'none';
     }
 
     if (scrollIndicatorEffect.style.left <= '99.9%' && scrollIndicatorEffect.style.display == 'none') {
-        console.log('ㅇㅇ');
         scrollIndicatorEffect.style.display = 'block';
     }
 }
@@ -45,14 +42,14 @@ function slider() {
         speed: 200, // 다음 버튼 누르고 다음 화면 뜨는데까지 걸리는 시간(ms)
         arrows: false, // 옆으로 이동하는 화살표 표시 여부
         dots: false, // 스크롤바 아래 점으로 페이지네이션 여부
-        autoplay: false, // 자동 스크롤 사용 여부
+        autoplay: true, // 자동 스크롤 사용 여부
         autoplaySpeed: 2000, // 자동 스크롤 시 다음으로 넘어가는데 걸리는 시간 (ms)
         pauseOnHover: false, // 슬라이드 이동	시 마우스 호버하면 슬라이더 멈추게 설정
         vertical: false, // 세로 방향 슬라이드 옵션
         prevArrow: "<button type='button' class='slick-prev'>Previous</button>", // 이전 화살표 모양 설정
         nextArrow: "<button type='button' class='slick-next'>Next</button>", // 다음 화살표 모양 설정
         dotsClass: 'slick-dots', //아래 나오는 페이지네이션(점) css class 지정
-        draggable: true, //드래그 가능 여부
+        draggable: false, //드래그 가능 여부
 
         responsive: [
             // 반응형 웹 구현 옵션
@@ -73,15 +70,6 @@ function slider() {
         ],
     });
 }
-
-// function navScrollEffect(target) {
-//     const targetTop = document.querySelector(target).offsetTop;
-
-//     window.scrollTo({
-//         top: targetTop,
-//         behavior: 'smooth',
-//     });
-// }
 
 function navScrollEffect(target) {
     const targetTop = document.querySelector(target).offsetTop;
@@ -170,7 +158,6 @@ function midLineAnimation() {
             midLine.style.top = headerHeight + 'px';
             midLine.style.left = '0'; //IE 오류 해결
             midLineAnimation = false;
-            console.log('아');
         }
         // scroll 이 중간을 안 넘었을 때
         if (!midLineAnimation && winY < midLineTop - headerHeight) {
@@ -178,7 +165,6 @@ function midLineAnimation() {
             midLine.style.position = 'relative';
             midLine.style.top = '0';
             midLineAnimation = true;
-            console.log('dh');
         }
     });
 }
